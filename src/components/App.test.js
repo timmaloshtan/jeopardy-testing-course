8770dd9dab3bf64940bab8505b7ce116d31b50af
Tim M <tim917@gmail.com>
2019-06-07 10:56:03 +0300

    Complete API tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,8 +4,17 @@ import { shallow } from 'enzyme';
 import { App } from './App';
 import { categories } from '../data/fixtures';
 
+const setCategories = jest.fn();
+const pickCategory = jest.fn();
+
 const props = {
   categories,
+  pickCategory,
+};
+
+const emptyProps = {
+  categories: [],
+  setCategories,
 };
 
 describe('App component', () => {
@@ -26,9 +35,44 @@ describe('App component', () => {
         (link, i) => expect(link.text()).toEqual(categories[i].title),
       );
     });
+
+    describe('and clicking on of the categories', () => {
+      beforeEach(() => {
+        app.find('Link').at(0).simulate('click');
+      });
+
+      it('should dispatch pickCategory action', () => {
+        expect(pickCategory.mock.calls.length).toBe(1);
+        expect(pickCategory.mock.calls[0][0]).toEqual(categories[0]);
+      });
+    });
   });
 
   describe('when rendering without categories', () => {
+    const mockSuccessResponse = categories;
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({ // 3
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    beforeEach(done => {
+      global.fetch.mockClear();
+      setCategories.mockClear();
+      app = shallow(<App {...emptyProps} />);
+
+      process.nextTick(() => done());
+    });
+
+    it('should fetch categories', () => {
+      expect(global.fetch.mock.calls.length).toBe(1);
+      expect(global.fetch.mock.calls[0][0]).toEqual('http://jservice.io/api/categories?count=20');
+    });
+    
+    it('should dispatch setCategories action', () => {
+      expect(setCategories.mock.calls.length).toBe(1);
+      expect(setCategories.mock.calls[0][0]).toEqual(categories);
+    })
     
   });
 });
